eba3a3a06aebbe1eba76fea5acfe0a26b843718a
Dia Soni <purplestudios7@gmail.com>
2026-06-04 17:43:13 +0530

    setup hero motion architecture

---
+++ src/components/Motion/transitions.ts
@@ -0,0 +1,19 @@
+export const HERO_REVEAL = {
+  duration: 0.8,
+  ease: [0.22, 1, 0.36, 1],
+};
+
+export const ROW_REVEAL = {
+  duration: 0.7,
+  ease: [0.22, 1, 0.36, 1],
+};
+
+export const CARD_REVEAL = {
+  duration: 0.6,
+  ease: [0.22, 1, 0.36, 1],
+};
+
+export const HOVER = {
+  duration: 0.4,
+  ease: [0.22, 1, 0.36, 1],
+};
---
+++ src/components/Motion/variants.ts
@@ -0,0 +1,7 @@
+export const heroReveal = {};
+
+export const rowReveal = {};
+
+export const imageCardHover = {};
+
+export const textCardHover = {};
